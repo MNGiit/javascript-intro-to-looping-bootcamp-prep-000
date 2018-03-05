668a85c30456d390aeaa60be554e1830e1832853
Mohammed Noori <kmanoorib@gmail.com>
2018-03-05 21:22:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop (array) {
   for ( var i = 0; i < 25; i++) {
-    console.log('I am ${i} strange loops.');
+    array.push(console.log('I am ${i} strange loops.');
   }
   return array;
 }
